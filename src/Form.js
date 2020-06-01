1385d0cf29a0a6dcb2399b7bda1e7d4ea780841c
maurosakugawa <maurotoshiuki@gmail.com>
2020-06-01 01:34:02 -0300

    correção do fetch - antes não estava fazendo o fetch, agora faz

--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Tabela from './Tabela';
+// import Tabela from './Tabela';
 
 export default class Form extends React.Component {
     constructor() {
@@ -7,61 +7,68 @@ export default class Form extends React.Component {
         this.state = {
             nome: '',
             cidades: '',
+            cidade: ''
         };
     }
 
-    changeNome = event => {
-        this.setState({
-            nome: event.target.value.toLowerCase()
-        });
-        if (this.state.nome.length >= 3) {
-            let cid = this.state.value;
-            fetch(`http://servicos.cptec.inpe.br/XML/listaCidades?city=${cid}`,{method:"GET"})
-            .then(response => response.arrayBuffer())
-            .then(buffer => {
-                let decoder = new TextDecoder("iso-8859-1");
-                return decoder.decode(buffer);
+    componentDidMount() {
+        fetch(`http://servicos.cptec.inpe.br/XML/listaCidades?city=${this.state.nome}`,{method:"GET"}, {
+            mode: 'no-cors',
+            method: 'GET',
+            headers: new Headers({
+                'Accept': 'application/xml',
+                'content-type': 'application/x-www-form-urlencoded',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, PUT',
+                'Access-Control-Allow-Headers': 'Content-Type',
             })
-            .then(str => {
-                let parser = new window.DOMParser()
-                let xml = parser.parseFromString(str, "text/xml");
-                let cidades = xml.getElementsByTagName('cidades')[0]; // Obter a tag cidades
-                let lista = [];
-                cidades.childNodes.forEach((obj, index) => {
-                    lista.push(obj.childNodes[0].childNodes[0].nodeValue)
-                });
-                console.log(lista);
-            })
-            .catch(erro => console.log(erro) );
-        }
-    };
+        })
+        .then(response => response.arrayBuffer())
+        .then(buffer => {
+            let decoder = new TextDecoder("iso-8859-1");
+            return decoder.decode(buffer);
+        })
+        .then(str => { console.log(str);
+            let parser = new window.DOMParser()
+            let xml = parser.parseFromString(str, "text/xml");
+            let cidades = xml.getElementsByTagName('cidades')[0]; // Obter a tag cidades
+            let lista = [];
+            cidades.childNodes.forEach((obj, index) => {
+                lista.push(obj.childNodes[0].childNodes[0].nodeValue)
+            });
+  //          console.log(str);
+        })
+        .catch(erro => console.log(erro) );
+    }
+    
 
     changeCidade = event => {
         
     }
 
     render() {
+
         return (
             <div>
                 <form onSubmit={this.submit} >
                     <div>
                         <label>Nome</label>
-                        <input type='text' name='idade' value={this.state.nome}
-                                onChange={this.changeNome} />
-                        <label>Cidade</label>
-                        <select name='cidade' value={this.state.cidade}
-                            onChange={this.changeCidade} >
-                            <option>{cidade}</option>
-                        </select>
+                        <input type='text' name='nome' value={this.state.nome}
+                            onChange={e => this.setState({
+                                nome: e.target.value })
+                            }
+                        />
+
+
                     </div>
                     <div>
                         {this.state.erroMsg}
                     </div>
                 </form>
-                <Tabela lista={this.state.cidade} />
+
             </div>
         );
-    }
+   }
 }
 
 
