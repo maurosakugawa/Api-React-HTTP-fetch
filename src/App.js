437204d1b8ed72f02153c07d5b28494f93cdc5ea
maurosakugawa <maurotoshiuki@gmail.com>
2020-06-12 11:50:23 -0300

    Removendo os console.log

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from './Form';
+
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Form />
+      </div>
+    );
+  }
+}
